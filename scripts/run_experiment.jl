dde9eafa83e3c7454fa434b0f71b876a1353c530
DrWatson <no@mail>
2021-10-18 18:48:40 +0200

    TEMPORARY DEPLOY COMMIT

--- scripts/run_experiment.jl
+++ scripts/run_experiment.jl
@@ -19,7 +19,7 @@ include("RLExp_BDQN_Atari.jl")
 
 games = ["breakout"]
 # experiments = [E`RLExp_BDQN_Atari($(game))` for game in games]
-experiments = [E`RLExp_BDQN_Atari(breakout)`]
+experiments = [E`RLExp_BDQN_Atari(pong)`]
 
 # pmap(run, experiments)
 run(experiments[1])
