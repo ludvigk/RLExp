ff6000e1d9f3afce94bb19563d059fb3c82e86ff
DrWatson <no@mail>
2021-10-18 18:10:27 +0200

    TEMPORARY DEPLOY COMMIT

--- src/algorithms/BDQN.jl
+++ src/algorithms/BDQN.jl
@@ -165,7 +165,7 @@ function RLBase.update!(learner::BDQNLearner, batch::NamedTuple)
         noisy_q = noisy_q + learner.injected_noise * randn!(similar(noisy_q))
         ent = entropy_surrogate(learner.sse, permutedims(noisy_q, (2, 1)))
         ce = sum(noisy_q .^ 2; dims = 2) ./ (2 * 5.0f0 .^ 2)
-        ce = mean(ce)
+        ce = sum(ce) / size(ce, 1)
         kl = -ent + ce
 
         Zygote.ignore() do
