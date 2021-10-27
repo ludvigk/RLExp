099a302dfc6bcdc8ca189e1e24e1259326b7de32
DrWatson <no@mail>
2021-10-27 13:58:19 +0200

    TEMPORARY DEPLOY COMMIT

--- scripts/run_experiment.jl
+++ scripts/run_experiment.jl
@@ -16,11 +16,12 @@ addprocs_slurm(n_workers; topology = :master_worker, exeflags=["--project=.", "-
 @everywhere using DrWatson
 @everywhere @quickactivate :RLExp
 include("RLExp_BDQN_Atari.jl")
+include("RLExp_GDQN_Atari.jl")
 include("Dopamine_DQN_Atari.jl")
 
 games = ["breakout"]
 # experiments = [E`RLExp_BDQN_Atari($(game))` for game in games]
-experiments = [E`RLExp_BDQN_Atari(pong)`]
+experiments = [E`RLExp_GDQN_Atari(pong)`]
 # experiments = [E`Dopamine_DQN_Atari(pong)`]
 
 # pmap(run, experiments)
