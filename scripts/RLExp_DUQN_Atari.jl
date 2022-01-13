f1f2e472f6c3d8ef1a6c5e2ee289e46436929114
DrWatson <no@mail>
2022-01-13 13:53:58 +0100

    TEMPORARY DEPLOY COMMIT

--- scripts/RLExp_DUQN_Atari.jl
+++ scripts/RLExp_DUQN_Atari.jl
@@ -51,7 +51,7 @@ function RL.Experiment(
                         "batch_size" => 32,
                         "min_replay_history" => 50000,
                         "updates_per_step" => 1,
-                        "λ" => 0,
+                        "λ" => 10,
                         "prior" => "GaussianPrior(0, 10)",
                         "n_samples" => 100,
                         "η" => 0.01,
