da2eed5ac778b2063ae4a22c4967d958e238520a
DrWatson <no@mail>
2022-03-07 13:44:21 +0100

    TEMPORARY DEPLOY COMMIT

--- scripts/run_experiment.jl
+++ scripts/run_experiment.jl
@@ -66,7 +66,7 @@ config7 = copy(config)
 config7["prior"] = "CartpolePrior(50; ν=-1)"
 confs = [config1, config2, config3, config4, config5, config6, config7]
 
-exs = [RL.Experiment(Val(:RLExp), Val(:DUQNS), Val(:Cartpole), "name"; config = conf) for conf in confs for _ in 1:10]
+exs = [RL.Experiment(Val(:RLExp), Val(:DUQNS), Val(:Cartpole), "name"; config = conf) for conf in confs for _ in 1:3]
 
 pmap(run, exs)
 # run(experiments[1])
