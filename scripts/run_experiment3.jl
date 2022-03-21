bcc31ac860d2e8ccae68a61cd4a4fe68fb390e54
DrWatson <no@mail>
2022-03-21 16:10:50 +0100

    TEMPORARY DEPLOY COMMIT

--- scripts/run_experiment3.jl
+++ scripts/run_experiment3.jl
@@ -19,6 +19,7 @@ addprocs_slurm(n_workers; topology = :master_worker, exeflags=["--project=.", "-
     using Distributed
     using DrWatson
     using RLExp
+    using Random
     include("../src/utils/gym.jl")
     install_gym()
     include("RLExp_DUQNS_LunarLander.jl")
