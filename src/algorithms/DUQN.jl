6f2081844a43c1c2d5bb7c1f30aa7b971d0446d3
Ludvig Killingberg <lk@vitalthings.com>
2022-05-12 10:11:05 +0200

    TEMPORARY DEPLOY COMMIT

--- src/algorithms/DUQN.jl
+++ src/algorithms/DUQN.jl
@@ -171,7 +171,7 @@ function RLBase.update!(learner::DUQNLearner, batch::NamedTuple)
         # k = KDE((cpu(reshape(b, :))))
         # 𝐿 = -sum(score_samples(b, G)) / (batch_size * n_samples)
 
-        # m = sum(b, dims=2) ./ size(b, 2)
+        m = sum(G, dims=2) ./ size(b, 2)
         ss = sum(G .^ 2, dims=2) ./ size(b, 2) .- m .^ 2
         𝐿 = sum(log.(ss) .+ (b .- G) .^ 2 ./ 2ss) / (batch_size .* n_samples)
 
