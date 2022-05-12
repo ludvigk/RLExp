f808b9b5ee1e63e6c5595645e07b7e7ead73fe7f
Ludvig Killingberg <lk@vitalthings.com>
2022-05-12 10:10:36 +0200

    TEMPORARY DEPLOY COMMIT

--- src/algorithms/DUQN.jl
+++ src/algorithms/DUQN.jl
@@ -169,11 +169,11 @@ function RLBase.update!(learner::DUQNLearner, batch::NamedTuple)
         b = b_all[a, :]
 
         # k = KDE((cpu(reshape(b, :))))
-        𝐿 = -sum(score_samples(b, G)) / (batch_size * n_samples)
+        # 𝐿 = -sum(score_samples(b, G)) / (batch_size * n_samples)
 
         # m = sum(b, dims=2) ./ size(b, 2)
-        # ss = sum(b .^ 2, dims=2) ./ size(b, 2) .- m .^ 2
-        # 𝐿 = sum(log.(ss) .+ (m .- G) .^ 2 ./ 2ss) / (batch_size .* n_samples)
+        ss = sum(b .^ 2, dims=2) ./ size(b, 2) .- m .^ 2
+        𝐿 = sum(log.(ss) .+ (b .- G) .^ 2 ./ 2ss) / (batch_size .* n_samples)
 
         b_rand = reshape(b_all, :, n_samples) ## SLOW
         b_rand = Zygote.@ignore b_rand .+ 0.01f0 .* CUDA.randn(size(b_rand)...)
