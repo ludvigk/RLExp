5b333ea34fca2d3109539798c5f75b99aed04b43
Ludvig Killingberg <lk@vitalthings.com>
2022-06-07 10:41:31 +0200

    TEMPORARY DEPLOY COMMIT

--- scripts/run_experiment.jl
+++ scripts/run_experiment.jl
@@ -20,7 +20,7 @@ addprocs_slurm(n_workers; topology=:master_worker, exeflags=["--project=.", "--c
     using Distributed
     using DrWatson
     using RLExp
-    include("RLExp_DUQN_Cartpole.jl")
+    include("RLExp_DUQNS_Cartpole.jl")
 end
 
 # include("RLExp_DUQN_Atari.jl")
