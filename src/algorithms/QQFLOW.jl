9d489f0ded69462647a947b92cb3628ef57f94fb
Ludvig Killingberg <lk@vitalthings.com>
2022-06-21 11:31:21 +0200

    TEMPORARY DEPLOY COMMIT

---
+++ src/algorithms/QQFLOW.jl
@@ -0,0 +1,184 @@
+export QFLOWLearner
+import ReinforcementLearning.RLBase.update!
+
+using DataStructures: DefaultDict
+using Distributions: Uniform, Product
+using StatsBase: sample
+using Flux.Losses
+import Statistics.mean
+
+Base.@kwdef struct FlowNetwork{B,F}
+    base::B
+    flow::F
+end
+
+Flux.@functor FlowNetwork
+
+function (m::FlowNetwork)(samples::AbstractMatrix, state::AbstractMatrix; reverse::Bool=true)
+    h = m.base(state)
+    return m.flow(samples, h; reverse)
+end
+
+function (m::FlowNetwork)(samples::AbstractArray{T,3}, state::AbstractMatrix; reverse::Bool=true) where {T}
+    h = m.base(state)
+    h = repeat(h, 1, 1, size(samples, 3))
+    return m.flow(samples, h; reverse)
+end
+
+mutable struct QFLOWLearner{
+    Tq<:AbstractApproximator,
+    Tt<:AbstractApproximator,
+    R<:AbstractRNG,
+} <: AbstractLearner
+    B_approximator::Tq
+    Q_approximator::Tt
+    num_actions::Int
+    Q_lr::Float32
+    min_replay_history::Int
+    B_update_freq::Int
+    Q_update_freq::Int
+    update_step::Int
+    n_samples_act::Int
+    n_samples_target::Int
+    sampler::NStepBatchSampler
+    rng::R
+    is_enable_double_DQN::Bool
+    training::Bool
+    logging_params
+end
+
+function QFLOWLearner(;
+    B_approximator::Tq,
+    Q_approximator::Tt,
+    num_actions::Int,
+    Q_lr::Real=0.01f0,
+    stack_size::Union{Int,Nothing}=nothing,
+    γ::Real=0.99f0,
+    batch_size::Int=32,
+    update_horizon::Int=1,
+    min_replay_history::Int=100,
+    B_update_freq::Int=1,
+    Q_update_freq::Int=1,
+    traces=SARTS,
+    update_step::Int=0,
+    n_samples_act::Int=30,
+    n_samples_target::Int=30,
+    is_enable_double_DQN::Bool=false,
+    training::Bool=true,
+    rng=Random.GLOBAL_RNG
+) where {Tq,Tt}
+    sampler = NStepBatchSampler{traces}(;
+        γ=Float32(γ),
+        n=update_horizon,
+        stack_size=stack_size,
+        batch_size=batch_size
+    )
+    return QFLOWLearner(
+        B_approximator,
+        Q_approximator,
+        num_actions,
+        Float32(Q_lr),
+        min_replay_history,
+        B_update_freq,
+        Q_update_freq,
+        update_step,
+        n_samples_act,
+        n_samples_target,
+        sampler,
+        rng,
+        is_enable_double_DQN,
+        training,
+        DefaultDict(0.0),
+    )
+end
+
+Flux.functor(x::QFLOWLearner) = (B=x.B_approximator, Q=x.Q_approximator),
+y -> begin
+    x = @set x.B_approximator = y.B
+    x = @set x.Q_approximator = y.Q
+    x
+end
+
+function (learner::QFLOWLearner)(env)
+    s = send_to_device(device(learner.B_approximator), state(env))
+    s = Flux.unsqueeze(s, ndims(s) + 1)
+    norm_samples = send_to_device(device(learner.B_approximator), randn(num_actions, size(s, 2), learner.n_samples_act))
+    q = dropdims(mean(learner.B_approximator(norm_samples, s; reverse=true), dims=3), dims=3)
+    vec(q) |> send_to_host
+end
+
+function RLBase.update!(learner::QFLOWLearner, t::AbstractTrajectory)
+    length(t[:terminal]) - learner.sampler.n <= learner.min_replay_history && return nothing
+
+    learner.update_step += 1
+
+    learner.update_step % learner.B_update_freq == 0 || learner.update_step % learner.Q_update_freq == 0 || return nothing
+
+    if learner.update_step % learner.B_update_freq == 0
+        _, batch = sample(learner.rng, t, learner.sampler)
+        update!(learner, batch)
+    end
+    if learner.update_step % learner.Q_update_freq == 0
+        η = learner.Q_lr
+        B = learner.B_approximator
+        Q = learner.Q_approximator
+        Bp = Flux.params(B)
+        Qp = Flux.params(Q)
+        if η == 1
+            Flux.loadparams!(Q, Bp)
+        else
+            p = Qp .- η .* (Qp .- Bp)
+            Flux.loadparams!(Q, p)
+        end
+    end
+end
+
+function RLBase.update!(learner::QFLOWLearner, batch::NamedTuple)
+    B = learner.B_approximator
+    Q = learner.Q_approximator
+    n_samples_target = learner.n_samples_target
+    γ = learner.sampler.γ
+    n = learner.sampler.n
+    batch_size = learner.sampler.batch_size
+    is_enable_double_DQN = learner.is_enable_double_DQN
+    D = device(Q)
+
+    s, a, r, t, s′ = (send_to_device(D, batch[x]) for x in SARTS)
+    a = CartesianIndex.(a, 1:batch_size)
+
+    norm_samples = randn(size(a)..., n_samples_target)
+    if is_enable_double_DQN
+        q_values = dropdims(mean(B(norm_samples, s′; reverse=true), dims=3), dims=3)
+    else
+        q_values = dropdims(mean(Q(norm_samples, s′; reverse=true), dims=3), dims=3)
+    end
+
+    if haskey(batch, :next_legal_actions_mask)
+        l′ = send_to_device(D, batch[:next_legal_actions_mask])
+        q_values .+= ifelse.(l′, 0.0f0, typemin(Float32))
+    end
+
+    if is_enable_double_DQN
+        selected_actions = dropdims(argmax(q_values; dims=1); dims=1)
+        q′ = dropdims(mean(Q(norm_samples, s′; reverse=true), dims=3), dims=3)
+        q′ = @inbounds q′[selected_actions]
+    else
+        q′ = dropdims(maximum(q_values; dims=1); dims=1)
+    end
+    G = Flux.unsqueeze(r .+ γ^n .* (1 .- t), dims=1) .* q′
+
+    gs = gradient(params(B)) do
+        preds, sldj = B(G, s; reverse=false)
+        ll = preds[a] .^ 2 ./ 2
+        𝐿 = sum(ll) - sum(sldj)
+        𝐿 = 𝐿 / batch_size
+
+        Zygote.ignore() do
+            learner.logging_params["𝐿"] = 𝐿
+            learner.logging_params["Qₜ"] = sum(G) / length(G)
+        end
+
+        return 𝐿
+    end
+    update!(B, gs)
+end
--- src/algorithms/normflows.jl
+++ src/algorithms/normflows.jl
@@ -1,9 +1,11 @@
 using Flux
+using Zygote
 using DataFrames
-# using SyntheticDatasets
-# using Plots
-# using StatsPlots
-# using ProgressMeter
+using SyntheticDatasets
+using Plots
+using StatsPlots
+using ProgressMeter
+
 import Distributions: logpdf
 
 export UvPlanar
@@ -153,7 +155,7 @@ function ConditionalCouplingLayer(in::Int, cond::Int, hidden::Int, mask)
     return ConditionalCouplingLayer(mask, net, RescaleLayer(in))
 end
 
-function (c::ConditionalCouplingLayer)(x, h::AbstractMatrix{T}, sldj=nothing; reverse=true) where {T}
+function (c::ConditionalCouplingLayer)(x, h::AbstractMatrix{T}, sldj=nothing; action=nothing, reverse=true) where {T}
     x_ = x .* c.mask
     x_h_ = vcat(x_, h)
     s, t = c.net(x_h_)
@@ -168,12 +170,16 @@ function (c::ConditionalCouplingLayer)(x, h::AbstractMatrix{T}, sldj=nothing; re
         return x
     else
         x = (x .+ t) .* exp.(s)
-        sldj += dropdims(sum(s, dims=1), dims=1)
+        if isnothing(action)
+            sldj += dropdims(sum(s, dims=1), dims=1)
+        else
+            sldj += s[action, :]
+        end
         return x, sldj
     end
 end
 
-function (c::ConditionalCouplingLayer)(x, h::AbstractArray{T,3}, sldj=nothing; reverse=true) where {T}
+function (c::ConditionalCouplingLayer)(x, h::AbstractArray{T,3}, sldj=nothing; action=nothing, reverse=true) where {T}
     x_ = x .* c.mask
     x_h_ = cat(repeat(x_, 1, 1, size(h, 3)), h, dims=1)
     x_h_ = reshape(x_h_, size(x_h_, 1), :)
@@ -189,7 +195,11 @@ function (c::ConditionalCouplingLayer)(x, h::AbstractArray{T,3}, sldj=nothing; r
         return reshape(x, size(x, 1), :, size(h, 3))
     else
         x = (x .+ t) .* exp.(s)
-        sldj += sum(s, dims=1)
+        if isnothing(action)
+            sldj += dropdims(sum(s, dims=1), dims=1)
+        else
+            sldj += s[action, :]
+        end
         return reshape(x, size(x, 1), :, size(h, 3)), reshape(sldj, :, size(h, 3))
     end
 end
@@ -224,7 +234,7 @@ end
 
 Flux.@functor ConditionalRealNVP
 
-function (r::ConditionalRealNVP)(x, h::AbstractMatrix; reverse=false)
+function (r::ConditionalRealNVP)(x, h::AbstractMatrix; action=nothing, reverse=false)
     if reverse
         for layer in r.coupling_layers
             x = layer(x, h; reverse)
@@ -234,13 +244,13 @@ function (r::ConditionalRealNVP)(x, h::AbstractMatrix; reverse=false)
         sldj = Zygote.@ignore similar(x, size(x, 2))
         Zygote.@ignore fill!(sldj, zero(eltype(x)))
         for layer in Base.reverse(r.coupling_layers)
-            x, sldj = layer(x, h, sldj; reverse)
+            x, sldj = layer(x, h, sldj; action, reverse)
         end
         return x, sldj
     end
 end
 
-function (r::ConditionalRealNVP)(x, h::AbstractArray{T,3}; reverse=false) where {T}
+function (r::ConditionalRealNVP)(x, h::AbstractArray{T,3}; action=nothing, reverse=false) where {T}
     if reverse
         for layer in r.coupling_layers
             x = layer(x, h; reverse)
@@ -250,54 +260,58 @@ function (r::ConditionalRealNVP)(x, h::AbstractArray{T,3}; reverse=false) where 
         sldj = Zygote.@ignore similar(x, size(x, 2), size(h, 3))
         Zygote.@ignore fill!(sldj, zero(eltype(x)))
         for layer in Base.reverse(r.coupling_layers)
-            x, sldj = layer(x, h, sldj; reverse)
+            x, sldj = layer(x, h, sldj; action, reverse)
         end
         return x, sldj
     end
 end
 
-# function main()
-#     moons = SyntheticDatasets.make_moons(n_samples=100; noise=0.05)
-#     data = Matrix(moons[:, 1:2])'
+function main()
 
-#     DTYPE = Float32
-#     # dist = MvNormal(zeros(DTYPE, 2), ones(DTYPE, 2))
+    moons = SyntheticDatasets.make_moons(n_samples=100; noise=0.05)
+    data = Matrix(moons[:, 1:2])'
 
-#     loss(td, data) = -mean(logpdf_forward(td, data))
+    DTYPE = Float32
+    # dist = MvNormal(zeros(DTYPE, 2), ones(DTYPE, 2))
 
-#     coupling_layers = [
-#         ConditionalCouplingLayer(2, 32, 100, [0, 1]),
-#         ConditionalCouplingLayer(2, 32, 100, [1, 0]),
-#         ConditionalCouplingLayer(2, 32, 100, [0, 1]),
-#         ConditionalCouplingLayer(2, 32, 100, [1, 0]),
-#     ]
-#     rnvp = ConditionalRealNVP(coupling_layers)
+    loss(td, data) = -mean(logpdf_forward(td, data))
 
-#     function loss(model, data)
-#         x, sldj = model(data, rand(32, size(data, 2)))
-#         nll = sum(x .^ 2) / 2 - sum(sldj)
-#         return nll / size(data, 2)
-#     end
+    coupling_layers = [
+        ConditionalCouplingLayer(2, 1, 100, [0, 1]),
+        ConditionalCouplingLayer(2, 1, 100, [1, 0]),
+        ConditionalCouplingLayer(2, 1, 100, [0, 1]),
+        ConditionalCouplingLayer(2, 1, 100, [1, 0]),
+    ]
+    rnvp = ConditionalRealNVP(coupling_layers)
 
-#     opt = ADAM(0.0001)
-#     function nf_train(td, data, opt, ps, epochs; batchsize=100)
-#         p = Progress(epochs, 1)
-#         for i ∈ 1:epochs
-#             d = Flux.Data.DataLoader(data, batchsize=batchsize)
-#             for minibatch = d
-#                 gs = gradient(() -> loss(td, minibatch), ps)
+    function loss(model, data)
+        action = [
+            CartesianIndex(rand(1:2), i) for i = 1:size(data, 2)
+        ]
+        x, sldj = model(data, rand(1, size(data, 2)); action)
+        nll = sum(x[action] .^ 2) / 2 - sum(sldj)
+        return nll / size(data, 2)
+    end
 
-#                 # println(gs.grads[b.ts[1].s[1].weight])
-#                 # println(gs.grads[b.ts[1].t[1].weight])
-#                 Flux.update!(opt, ps, gs)
-#             end
-#             ProgressMeter.next!(p; showvalues=[(:nll, loss(td, data))])
-#         end
-#     end
-#     nf_train(rnvp, data, opt, Flux.params(rnvp), 5000)
-#     # contour(-2:0.1:3, -2:0.1:2, (x, y) -> pdf(td, [x, y]), fill=true)
-#     # samples = rand(td, 100)
-#     samples = rnvp(randn(2, 100), rand(32, 100), reverse=true)
-#     @df moons scatter(:feature_1, :feature_2)
-#     scatter!(samples[1, :], samples[2, :])
-# end
+    opt = ADAM(0.0003)
+    function nf_train(td, data, opt, ps, epochs; batchsize=100)
+        p = Progress(epochs, 1)
+        for i ∈ 1:epochs
+            d = Flux.Data.DataLoader(data, batchsize=batchsize)
+            for minibatch = d
+                gs = gradient(() -> loss(td, minibatch), ps)
+
+                # println(gs.grads[b.ts[1].s[1].weight])
+                # println(gs.grads[b.ts[1].t[1].weight])
+                Flux.update!(opt, ps, gs)
+            end
+            ProgressMeter.next!(p; showvalues=[(:nll, loss(td, data))])
+        end
+    end
+    nf_train(rnvp, data, opt, Flux.params(rnvp), 5000)
+    # contour(-2:0.1:3, -2:0.1:2, (x, y) -> pdf(td, [x, y]), fill=true)
+    # samples = rand(td, 100)
+    samples = rnvp(randn(2, 100), rand(1, 100), reverse=true)
+    @df moons scatter(:feature_1, :feature_2)
+    scatter!(samples[1, :], samples[2, :])
+end
