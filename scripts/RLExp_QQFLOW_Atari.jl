a2db2bcfb18299fe5225258ea8e32aad27838a7d
DrWatson <no@mail>
2022-07-19 13:49:23 +0200

    TEMPORARY DEPLOY COMMIT

--- scripts/RLExp_QQFLOW_Atari.jl
+++ scripts/RLExp_QQFLOW_Atari.jl
@@ -43,14 +43,14 @@ function RL.Experiment(
     """
     if isnothing(config)
         config = Dict(
-            "B_lr" => 1e-4,
+            "B_lr" => 5e-5,
             "Q_lr" => 1,
             "B_clip_norm" => 10.0,
             "B_update_freq" => 4,
             "Q_update_freq" => 10_000,
             "n_samples_act" => 100,
-            "n_samples_target" => 1,
-            "hidden_dim" => 32,
+            "n_samples_target" => 100,
+            "hidden_dim" => 16,
             "B_opt" => "ADAM",
             "gamma" => 0.99,
             "update_horizon" => 1,
@@ -128,10 +128,18 @@ function RL.Experiment(
                 PlanarLayer(N_ACTIONS, hidden_dim, 32),
                 PlanarLayer(N_ACTIONS, hidden_dim, 32),
                 PlanarLayer(N_ACTIONS, hidden_dim, 32),
-                # PlanarLayer(N_ACTIONS, hidden_dim, 32),
-                # PlanarLayer(N_ACTIONS, hidden_dim, 32),
-                # PlanarLayer(N_ACTIONS, hidden_dim, 32),
-                # PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
         ]
         )
 
@@ -141,10 +149,18 @@ function RL.Experiment(
                 PlanarLayer(N_ACTIONS, hidden_dim, 32),
                 PlanarLayer(N_ACTIONS, hidden_dim, 32),
                 PlanarLayer(N_ACTIONS, hidden_dim, 32),
-                # PlanarLayer(N_ACTIONS, hidden_dim, 32),
-                # PlanarLayer(N_ACTIONS, hidden_dim, 32),
-                # PlanarLayer(N_ACTIONS, hidden_dim, 32),
-                # PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
+                PlanarLayer(N_ACTIONS, hidden_dim, 32),
         ]
         )
 
