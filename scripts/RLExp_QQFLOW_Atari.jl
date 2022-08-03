27ef9ed8e9604fc0b716aff4ed7846cb14c9bcf7
DrWatson <no@mail>
2022-08-03 15:00:24 +0200

    TEMPORARY DEPLOY COMMIT

--- scripts/RLExp_QQFLOW_Atari.jl
+++ scripts/RLExp_QQFLOW_Atari.jl
@@ -61,7 +61,7 @@ function RL.Experiment(
             "traj_capacity" => 1_000_000,
             "seed" => 1,
             "flow_width" => 16,
-            "terminal_on_life_loss" => true,
+            "terminal_on_life_loss" => false,
         )
     end
 
