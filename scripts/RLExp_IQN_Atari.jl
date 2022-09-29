95053a8e33e0025b45748ea0e0a268540f404d4a
Ludvig Killingberg <lk@vitalthings.com>
2022-09-29 15:06:45 +0200

    TEMPORARY DEPLOY COMMIT

--- scripts/RLExp_IQN_Atari.jl
+++ scripts/RLExp_IQN_Atari.jl
@@ -198,7 +198,7 @@ function RL.Experiment(
             ϕ=Dense(Nₑₘ => 11 * 11 * 64, relu; init=init),
             header=Chain(
                 Dense(11 * 11 * 64 => 512, relu; init=init),
-                Dense(512 => N_ACTIONS; init=init),
+                Dense(512 => 10 * N_ACTIONS; init=init),
             ),
         ) |> gpu
 
@@ -219,6 +219,7 @@ function RL.Experiment(
                 κ=1.0f0,
                 N=64,
                 N′=64,
+                Aₑₘ=10,
                 Nₑₘ=Nₑₘ,
                 K=32,
                 γ=0.99f0,
